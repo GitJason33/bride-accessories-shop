08923ac34319de1579ee057a3c8edb1b3759fadb
GitJason33 <121455569+GitJason33@users.noreply.github.com>
2023-02-26 17:35:45 +0200

    Add files via upload

---
+++ links.js
@@ -0,0 +1,71 @@
+// image slider links
+const homeImages = [
+  "crown1.jpg",
+  "flower1.jpg",
+  "headband1.jpg",
+  "haircomb1.jpg",
+  "bracelet1.jpg",
+  "glass1.jpg",
+  "cr-rita.jpg",
+  "co-rita.jpg"
+];
+
+// link versions for categories
+const links_v1 = [ // home version
+  // logo 0
+  "../media/images/logo.png",
+
+  // home 1 - 5
+  "./home.html",
+  "./aboutus.html",
+  "./special-offers.html",
+  "./news.html",
+  "./decor-items.html",
+
+  // categories 6+
+  "./categories/crowns/crowns.html",
+  "./categories/flowers/flowers.html",
+  "./categories/headbands/headbands.html",
+  "./categories/haircombs/haircombs.html",
+  "./categories/glasses/toastglass.html",
+  "./categories/bracelets/bracelets.html",
+  "./categories/rita-menu/rita.html",
+
+  // javscript & css links from home
+  "./home.css",
+  "./home.js",
+
+  // language
+  "../ar/home.html"
+];
+
+const links_v2 = [ // categories version
+  // logo
+  "../../../media/images/logo.png",
+
+  // home
+  "../../home.html",
+  "../../aboutus.html",
+  "../../special-offers.html",
+  "../../news.html",
+  "../../decor-items.html",
+
+  // ../varying/cat.html
+  // categories - to be fixed with a complex function
+  "../crowns/crowns.html",
+  "../flowers/flowers.html",
+  "../headbands/headbands.html",
+  "../haircombs/haircombs.html",
+  "../glasses/toastglass.html",
+  "../bracelets/bracelets.html",
+  "../rita-menu/rita.html",
+
+  // javscript & css links from home
+  "../../home.css",
+  "../../home.js",
+
+  // language
+  "../../../ar/home.html"
+];
+
+export { homeImages, links_v1, links_v2 };
