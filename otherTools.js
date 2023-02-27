cfeaa64fbf2d6f5f7f7a2d69547c41e9eec09c9a
Sukojay <sukojay373@gmail.com>
2023-02-27 15:21:13 +0200

    remove arabic lang

---
+++ otherTools.js
@@ -0,0 +1,67 @@
+// check the image folder
+const imageDirectory = {
+  crown: "crowns",
+  headband: "headband",
+  haircomb: "hair comb",
+  flower: "flowers",
+  glass: "decorated glasses",
+  bracelet: "bracelet",
+  rita_crown: "rita-crowns",
+  rita_comb: "rita-combs"
+};
+
+// 8 categories available
+// available category pages 
+const pgCrown = [
+  "crowns.html",
+  "crowns 2.html",
+  "crowns 3.html",
+  "crowns 4.html"
+];
+const pgBracelet = [
+  "bracelets.html"
+];
+const pgFlower = [
+  "flowers.html",
+  "flowers2.html",
+  "flowers3.html"
+];
+const pgHeadband = [
+  "headbands.html",
+  "headbands 2.html",
+  "headbands 3.html",
+  "headbands 4.html",
+  "headbands 5.html"
+];
+const pgGlass = [
+  "toastglass.html"
+];
+const pgHaircomb = [
+  "haircombs.html",
+  "haircombs 2.html",
+  "haircombs 3.html"
+];
+const pgCrRita = [
+  "cr-rita 1.html",
+  "cr-rita 2.html",
+  "cr-rita 3.html",
+  "cr-rita 4.html"
+];
+const pgCoRita = [
+  "co-rita 1.html",
+  "co-rita 2.html",
+  "co-rita 3.html",
+  "co-rita 4.html"
+];
+
+export { 
+  imageDirectory,
+  pgCrown,
+  pgBracelet, 
+  pgFlower, 
+  pgHeadband, 
+  pgGlass, 
+  pgHaircomb, 
+  pgCoRita, 
+  pgCrRita 
+};
